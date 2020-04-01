51059253545a4a495c4021a83deaa10224ed482f
Leandro Sabatini <contato@leandrosabatini.com.br>
2020-03-31 22:08:03 -0300

    crud de previsao do tempo

---
+++ src/main/resources/import.sql
@@ -0,0 +1,7 @@
+insert into Previsao(id, diaSemana, tempMin, tempMax, umidade, descricao) values (1, 'segunda feira', 18.0, 27.0, 30, 'Céu parcialmente nublado');
+insert into Previsao(id, diaSemana, tempMin, tempMax, umidade, descricao) values (2, 'terça feira'  , 19.0, 27.0, 43, 'Céu parcialmente nublado');
+insert into Previsao(id, diaSemana, tempMin, tempMax, umidade, descricao) values (3, 'quarta feira' , 19.0, 30.0, 65, 'Possibilidade de chuva');
+insert into Previsao(id, diaSemana, tempMin, tempMax, umidade, descricao) values (4, 'quinta feira' , 18.0, 26.0, 82, 'Chuva fraca');
+insert into Previsao(id, diaSemana, tempMin, tempMax, umidade, descricao) values (5, 'sexta feira'  , 16.0, 25.0, 43, 'Céu parcialmente nublado');
+insert into Previsao(id, diaSemana, tempMin, tempMax, umidade, descricao) values (6, 'sábado'       , 16.0, 27.0, 40, 'Céu parcialmente nublado');
+insert into Previsao(id, diaSemana, tempMin, tempMax, umidade, descricao) values (7, 'domingo'      , 18.0, 28.0, 75, 'Chuva com probabilidade de trovoada');
